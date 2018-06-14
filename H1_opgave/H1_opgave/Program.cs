de15dcbdc6af45cfc5074f5be40fd6d5cd6e3452
Jakob Vestergaard <jakobmetalv@gmail.com>
2018-06-14 11:29:54 +0200

    sdw

--- H1_opgave/H1_opgave/Program.cs
+++ H1_opgave/H1_opgave/Program.cs
@@ -10,9 +10,7 @@ namespace H1_opgave
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bette stift");
-            Console.WriteLine("huaa");
-            Console.WriteLine("wuaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah");
+           
         }
     }
 }
