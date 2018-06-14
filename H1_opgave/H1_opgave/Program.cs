72fb707bfc3feabb8e619ebfccb4bb7b5802a627
Jakob Vestergaard <jakobmetalv@gmail.com>
2018-06-14 11:35:21 +0200

    ss

--- H1_opgave/H1_opgave/Program.cs
+++ H1_opgave/H1_opgave/Program.cs
@@ -10,7 +10,7 @@ namespace H1_opgave
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Du er gay");
+            Console.WriteLine("Du er gays");
         }
     }
 }
